aca89fc3ff2763a7f594b8fee36cd0fb1265a9a2
Purva Fatima <Purvafatima90@gmail.com>
2025-09-01 12:32:57 +0500

    Added Main content for skeleton of the project

---
+++ src/layout/MainContent.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MainContent = ({ children, className = '', ...props }) => {
+  return (
+    <main 
+      className={`flex-1 bg-gray-50 p-4 sm:p-6 ml-0 sm:ml-64 transition-all duration-300 ${className}`}
+      {...props}
+    >
+      {children}
+    </main>
+  );
+};
+
+MainContent.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+};
+
+export default MainContent;
