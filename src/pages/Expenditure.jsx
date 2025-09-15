9e66121aaf459f8554b33d1f0bffb4dbc791ac1f
Purva Fatima <Purvafatima90@gmail.com>
2025-09-15 14:01:01 +0500

    Added expenditures page

---
+++ src/pages/Expenditure.jsx
@@ -0,0 +1,8 @@
+export default function Expenditure() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Expenditure Page</h1>
+      <p>This is the Expenditure page content.</p>
+    </div>
+  );
+}
